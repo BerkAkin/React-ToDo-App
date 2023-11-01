a574e48e546c880308449b82e89ef61c860f524a
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2023-11-01 18:21:36 +0300

    feat: todos now can be edited

--- src/components/List/index.js
+++ src/components/List/index.js
@@ -1,6 +1,10 @@
 import React from 'react'
+import { useEffect } from 'react';
+import { useState } from 'react';
 function List({todos, setTodos, filter}) {
 
+
+  
 
     const filteredTodos =
     filter === "active"
@@ -26,6 +30,50 @@ function List({todos, setTodos, filter}) {
     }
 
 
+    const onLabelClick = (id) =>{
+
+        let tr = todos.some((todo)=>todo.editable===true)
+
+        if(!tr){
+            todos.map((todo)=>{
+                if(todo.id === id){
+                    todo.editable = !todo.editable  
+                    setEditText(todo.text)
+                }
+            })
+        }
+        setTodos([...todos])
+    }
+
+
+
+
+    const [editText,setEditText] = useState('')
+
+    const onEditChange = (e) =>{
+            setEditText(e.target.value)
+        
+    }
+
+
+
+
+    const handleChange = (e , id) => {
+        const obje = todos.find(todo=>todo.id === id)
+        if(e.key === 'Enter'){
+            obje.text = editText
+            obje.editable = !obje.editable;
+            setEditText('')
+            if(obje.status === true){
+                obje.status = !obje.status
+            } 
+            setTodos([...todos])
+        }
+        
+    
+    }
+
+
   return (
     <>
         {
@@ -34,7 +82,13 @@ function List({todos, setTodos, filter}) {
                     <li key={todo.id} className={todo.status ? "completed" : ""}>
                         <div className="view">
                             <input onChange={()=>onCheck(todo.id)} checked={todo.status} className="toggle" type="checkbox" />
-                            <label>{todo.text}</label>
+                            {
+                            todo.editable 
+                            ? 
+                            <input onKeyUp={(e)=>handleChange(e,todo.id)} onChange={onEditChange} value={editText}/> 
+                            : 
+                            <label onClick={()=>onLabelClick(todo.id)}>{todo.text}</label>
+                            }
                             <button onClick={()=>onDelete(todo.id)} className="destroy"></button>
                         </div>
                     </li>
