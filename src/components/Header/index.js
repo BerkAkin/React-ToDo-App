24762bd491010fc9b515d8e27341508f82f1bb0b
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2023-11-01 20:22:37 +0300

    feat: todos are now don't accept empty values

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,26 +1,40 @@
 import React from 'react'
-function Header({todoText, setTodoText, todos, setTodos}) {
+
+
+function Header({todoText, setTodoText, todos, setTodos, error, setError}) {
+
+
 
 const handleChange = (e) =>{
   setTodoText({...todoText, 'text':e.target.value, 'id': Math.random()});
 } 
 const onEnter = (e)=>{
-  if(e.key === 'Enter'){
-    setTodos([...todos, todoText]);
-    setTodoText({...todoText, 'text':''});
+    if(e.key === 'Enter'){
+      if(e.target.value===""){
+        setError('Boş giriş yapılamaz !');
+        return
+      }
+      setTodos([...todos, todoText]);
+      setTodoText({...todoText, 'text':''})
+      setError("");
   }
+
+  
 }
 
 const onSubmitForm = (e) => {
   e.preventDefault();
 }
 
+
   return (
     <div>
          <header className="header">
                 <h1>Yapılacaklar</h1>
+                
+                <small style={{color:'red',display:'flex',justifyContent:'center',fontWeight:600}}>{error}</small>
                 <form onSubmit={onSubmitForm}>
-                    <input onKeyDown={onEnter} onChange={handleChange} value={todoText.text} className="new-todo" placeholder="Ne yapıyorsun?" />
+                    <input onKeyUp={(e)=>onEnter(e)} onChange={handleChange} value={todoText.text} className="new-todo" placeholder="Ne yapıyorsun?" />
                 </form>
             </header>
     </div>
